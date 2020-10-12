7ae5c4b8e664e5ecd453a94b5a97ee01b56bc8f3
Israel Parodi <israelps97@gmail.com>
2020-10-12 15:28:31 -0500

    Initial Commit

---
+++ ProyectoFinaliOS_Parodi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoFinaliOS_Parodi
+//
+//  Created by Israel Parodi Schmidt on 10/12/20.
+//  Copyright © 2020 Israel Parodi Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
